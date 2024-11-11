1ecf4945b177f1d5d120a6d794286ef5ce128f2a
Porter Schollenberger <porterschollenberger@gmail.com>
2024-11-10 20:44:17 -0700

    add(Ticker)

---
+++ src/components/Ticker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Ticker() {
+    return (
+        <div className="ticker">
+            <p>Random fact for entertainment sliding across the bottom of the screen</p>
+        </div>
+    );
+}
+
+export default Ticker;
