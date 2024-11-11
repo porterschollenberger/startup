95e26f1257b777438c4565bfc36a4badc695e1c3
Porter Schollenberger <porterschollenberger@gmail.com>
2024-11-10 20:38:25 -0700

    add(Home)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <main>
+            <div className="form-container">
+                <div className="form-section">
+                    <h2>Login</h2>
+                    <form>
+                        <label htmlFor="loginEmail">Email</label>
+                        <input type="email" name="loginEmail" id="loginEmail" placeholder="example@email.com" required />
+                        <label htmlFor="loginPassword">Password</label>
+                        <input type="password" name="loginPassword" id="loginPassword" placeholder="Password" required />
+                        <input type="submit" value="Login" />
+                    </form>
+                </div>
+                <div className="form-section">
+                    <h2>Register</h2>
+                    <form>
+                        <label htmlFor="registerEmail">Email</label>
+                        <input type="email" name="registerEmail" id="registerEmail" placeholder="example@email.com" required />
+                        <label htmlFor="registerPassword">Password</label>
+                        <input type="password" name="registerPassword" id="registerPassword" placeholder="Password" required />
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" required />
+                        <input type="submit" value="Register" />
+                    </form>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Home;
