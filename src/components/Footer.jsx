dd3171143db522df2c196c01517341009fd3c9a8
Porter Schollenberger <porterschollenberger@gmail.com>
2024-11-10 20:36:34 -0700

    add(Footer)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>A game by Porter Schollenberger - <a href="https://github.com/porterschollenberger/startup">GitHub</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
