722e59c49a6381208afc0cdd26525cae505ebf85
Porter Schollenberger <porterschollenberger@gmail.com>
2024-11-10 20:35:33 -0700

    add(Navigation)

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+    return (
+        <nav>
+            <i className="fa-solid fa-bars"></i>
+            <ul className="nav-list">
+                <li><Link to="/play">Play</Link></li>
+                <li><Link to="/leaderboard">Leaderboard</Link></li>
+                <li><Link to="/achievements">Achievements</Link></li>
+            </ul>
+            <ul className="account-info">
+                <li><Link to="/">Logout</Link></li>
+                <li className="username">username</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
