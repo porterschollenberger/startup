93e43ed3dd87d8ea4105a7b9e509f95606b90f03
Porter Schollenberger <porterschollenberger@gmail.com>
2024-11-23 21:51:49 -0700

    update(Play.jsx) removed useless useRef

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGame } from '../GameContext';
 import './Play.css';
