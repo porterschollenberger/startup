d86cf8bb586c7ebb17cc65f98134421ec4595fe8
Porter Schollenberger <porterschollenberger@gmail.com>
2024-11-23 19:15:52 -0700

    update(index.jsx) use the new AuthProvider

--- index.jsx
+++ index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './src/app';
+import {AuthProvider} from "./src/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
