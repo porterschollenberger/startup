7617aa79ea1fbfb5d5d3c3f89c2d85f6be1eaa05
Porter Schollenberger <porterschollenberger@gmail.com>
2024-12-07 18:37:27 -0700

    fix(vite.config.js) ws was not set up correctly

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': 'http://localhost:3000',
-            'ws': {
-                target: 'ws.localhost:3000',
+            '/ws': {
+                target: 'ws://localhost:3000',
                 ws: true,
             },
         },
